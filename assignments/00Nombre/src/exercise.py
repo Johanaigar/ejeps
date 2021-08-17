36280816351c7c4141d203035aee2c8471aa7f0c
Johanaigar <johana.ig@culiacan.tecnm.mx>
2021-08-17 17:34:55 +0000

    nombre

---
+++ assignments/00Nombre/src/exercise.py
@@ -0,0 +1,10 @@
+def main():
+    #escribe tu código abajo de esta línea
+    pass
+    nombre=input("Dame tu nombre:")
+    apellido_paterno =input("Dame tu apellido Paterno :")
+    apellido_materno=input("Dame tu apellido Materno :")
+    nombre_completo=nombre + " " + apellido_paterno + " " + apellido_materno
+    print("Es un gusto conocerte " + nombre_completo)
+if __name__=='__main__':
+    main()
